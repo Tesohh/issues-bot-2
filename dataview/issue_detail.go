5d65c7e3cf4cd225b61c630d613ba088445083d6
Simone Tesini <simozilla006@gmail.com>
2025-09-18 00:39:02 +0200

    add buttons (not yet working) on issues

--- dataview/issue_detail.go
+++ dataview/issue_detail.go
@@ -53,11 +53,64 @@ func MakeIssueMainDetail(issue *db.Issue, nobodyRoleID string) dg.Container {
 	return container
 }
 
+func makeIssueNextStateButton(issue *db.Issue) dg.Button {
+	label := ""
+	style := dg.SecondaryButton
+	status := db.IssueStatusTodo
+	disabled := false
+	emoji := ""
+
+	switch issue.Status {
+	case db.IssueStatusTodo:
+		label = "Mark as Doing"
+		// style = dg.PrimaryButton
+		status = db.IssueStatusDoing
+		emoji = db.IssueStatusIcons[db.IssueStatusDoing]
+	case db.IssueStatusDoing:
+		label = "Mark as Done"
+		// style = dg.SuccessButton
+		status = db.IssueStatusDone
+		emoji = db.IssueStatusIcons[db.IssueStatusDone]
+	case db.IssueStatusDone:
+		label = "Revert to Todo"
+		// style = dg.SecondaryButton
+		status = db.IssueStatusTodo
+		emoji = db.IssueStatusIcons[db.IssueStatusTodo]
+	case db.IssueStatusCancelled:
+		label = "Mark as Cancelled"
+		// style = dg.DangerButton
+		status = db.IssueStatusCancelled
+		emoji = db.IssueStatusIcons[db.IssueStatusCancelled]
+		disabled = true
+	}
+
+	return dg.Button{
+		Label:    label,
+		Style:    style,
+		CustomID: fmt.Sprintf("issue-set-status:%d:%d", issue.ID, status),
+		Disabled: disabled,
+		Emoji:    &dg.ComponentEmoji{Name: emoji},
+	}
+}
+
+func makeAssignMeButton(issue *db.Issue) dg.Button {
+	return dg.Button{
+		Label:    "Assign me",
+		Style:    dg.PrimaryButton,
+		CustomID: fmt.Sprintf("issue-toggle-author-assignee:%d", issue.ID),
+		Disabled: false,
+	}
+}
+
 func MakeIssueThreadDetail(issue *db.Issue, nobodyRoleID string) []dg.MessageComponent {
 	allComponents := []dg.MessageComponent{
-		// ...
 		MakeIssueMainDetail(issue, nobodyRoleID),
-		// ...
+		dg.ActionsRow{
+			Components: []dg.MessageComponent{
+				makeIssueNextStateButton(issue),
+				makeAssignMeButton(issue),
+			},
+		},
 	}
 
 	return allComponents
