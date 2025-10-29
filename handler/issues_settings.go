ef9f5be96cc08bfbe7a1c9afa9048dbc3203b7c7
Simone Tesini <simozilla006@gmail.com>
2025-10-29 17:09:38 +0100

    temporarily comment issuesFilterPeople

--- handler/issues_settings.go
+++ handler/issues_settings.go
@@ -69,44 +69,29 @@ func issuesOrder(s *dg.Session, i *dg.InteractionCreate, args []string) error {
 }
 
 func issuesFilterPeople(s *dg.Session, i *dg.InteractionCreate, args []string) error {
-	err := s.InteractionRespond(i.Interaction, &dg.InteractionResponse{
-		Type: dg.InteractionResponseModal,
-		Data: &dg.InteractionResponseData{
-			CustomID: "issues_filter_people_submit:" + i.Message.ChannelID,
-			Title:    "Filter people",
-			Flags:    dg.MessageFlagsIsComponentsV2,
-			Components: []dg.MessageComponent{
-				dg.ActionsRow{
-					Components: []dg.MessageComponent{
-						dg.SelectMenu{
-							MenuType:    dg.StringSelectMenu,
-							CustomID:    "recruiters",
-							Placeholder: "Recruiters",
-							Options: []dg.SelectMenuOption{
-								{
-									Label:       "TESOHH",
-									Value:       "tesohh",
-									Description: "tesohh",
-								},
-							},
-						},
-					},
-				},
-				dg.ActionsRow{
-					Components: []dg.MessageComponent{
-						dg.TextInput{
-							CustomID:  "suggestions",
-							Label:     "What would you suggest to improve them?",
-							Style:     dg.TextInputParagraph,
-							Required:  false,
-							MaxLength: 2000,
-						},
-					},
-				},
-			},
-		},
-	})
-	return err
+	// err := s.InteractionRespond(i.Interaction, &dg.InteractionResponse{
+	// 	Type: dg.InteractionResponseModal,
+	// 	Data: &dg.InteractionResponseData{
+	// 		CustomID: "issues_filter_people_submit:" + i.Message.ChannelID,
+	// 		Title:    "Filter people",
+	// 		Flags:    dg.MessageFlagsIsComponentsV2,
+	// 		Components: []dg.MessageComponent{
+	// 			dg.Label{
+	// 				Label: "Recruiters",
+	// 				Component: dg.SelectMenu{
+	// 					MinValues:   slash.Ptr(0),
+	// 					MenuType:    dg.UserSelectMenu,
+	// 					CustomID:    "recruiters",
+	// 					Placeholder: "filter by recruiters...",
+	// 					MaxValues:   3,
+	// 					Required:    false,
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// })
+	// return err
+	return nil
 }
 
 func issuesFilterData(s *dg.Session, i *dg.InteractionCreate, args []string) error {
